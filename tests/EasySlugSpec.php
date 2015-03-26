b0460cfd1ab29dc8b763bb0d3aae621382b9c0ff
Tomasz Cieciński <ciecinski@gmail.com>
2015-03-26 22:07:14 +0100

    add comment and test

--- tests/EasySlugSpec.php
+++ tests/EasySlugSpec.php
@@ -13,6 +13,8 @@ class EasySlugSpec extends ObjectBehavior
     function it_should_return_empty_slug()
     {
         $this->create('')->format()->shouldReturn('');
+
+        $this::get('')->format()->shouldReturn('');
     }
 
     function it_should_return_plain_slug()
@@ -22,6 +24,8 @@ class EasySlugSpec extends ObjectBehavior
         $this->create('To --  jest --  slug')->format()->shouldReturn('To-jest-slug');
 
         $this->create('To --  jest --  slug')->plain()->shouldReturn('To-jest-slug');
+
+        $this::get('To jest slug')->__toString()->shouldReturn('To-jest-slug');
     }
 
     public function it_should_returns_slug_without_special_chars()
@@ -29,12 +33,16 @@ class EasySlugSpec extends ObjectBehavior
         $this->create('Tó i ówdzię jeśliś Slugiem')->format()->shouldReturn('To-i-owdzie-jeslis-Slugiem');
         $this->create('To 11 jest slug.')->format()->shouldReturn('To-11-jest-slug');
         $this->create('This is slug.!@#$%^&*(*)-')->format()->shouldReturn('This-is-slug');
+
+        $this::get('This is slug.!@#$%^&*(*)-')->__toString()->shouldReturn('This-is-slug');
     }
 
     function it_should_returns_formatted_slug()
     {
         $this->create('To jest slug')->format('%s-%d', [11])->shouldReturn('To-jest-slug-11');
         $this->create('To jest slug')->format('%s-%d.html', [11])->shouldReturn('To-jest-slug-11.html');
+
+        $this::get('To jest slug')->format('%s-%d.html', [11])->shouldReturn('To-jest-slug-11.html');
     }
 
     function it_should_return_slug_with_custom_replacement()
@@ -47,6 +55,9 @@ class EasySlugSpec extends ObjectBehavior
 
         $this->setReplacement('+');
         $this->create('Tó i ówdzię jeśliś Slugiem')->format()->shouldReturn('To+i+owdzie+jeslis+Slugiem');
+
+        
+        $this::get('Tó i ówdzię jeśliś Slugiem', '+')->__toString()->shouldReturn('To+i+owdzie+jeslis+Slugiem');
     }
 
 }
