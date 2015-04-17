6474a21df7ed836f5f4ac0ef58fa3e91efe74aa4
Pyton <pyton000@gmail.com>
2015-04-17 11:12:05 +0200

    Added French rules

---
+++ src/Rules/FrenchRule.php
@@ -0,0 +1,49 @@
+<?php namespace EasySlug\Rules;
+
+class FrenchRule extends RuleContract
+{
+
+    /**
+     * Set of replacements
+     * @var array
+     */
+    protected $rules = array(
+        // French
+        'à' => 'a',
+        'â' => 'a',
+        'ç' => 'c',
+        'é' => 'e',
+        'è' => 'e',
+        'ê' => 'e',
+        'ë' => 'e',
+        'î' => 'i',
+        'ï' => 'i',
+        'ô' => 'o',
+        'û' => 'u',
+        'ù' => 'u',
+        'ü' => 'u',
+        'ÿ' => 'y',
+        'À' => 'A',
+        'Â' => 'A',
+        'Ç' => 'C',
+        'É' => 'E',
+        'È' => 'E',
+        'Ê' => 'E',
+        'Ë' => 'E',
+        'Î' => 'I',
+        'Ï' => 'I',
+        'Ô' => 'O',
+        'Û' => 'U',
+        'Ù' => 'U',
+        'Ü' => 'U',
+        'Ÿ' => 'Y',
+    );
+
+    /**
+     * Set rules to parent class
+     */
+    public function __construct()
+    {
+        parent::__construct($this->rules);
+    }
+}
